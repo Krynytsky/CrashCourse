e15f384666f98820076c568383f2243129ac60d6
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-10-23 17:16:54 +0300

    stuck on task2(88)b

--- src/com/homework/Task2.java
+++ src/com/homework/Task2.java
@@ -3,6 +3,8 @@ package com.homework;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Task2 {
     public static void main(String[] args) throws IOException {
@@ -24,6 +26,9 @@ public class Task2 {
         } else {
             System.out.println("the number we had squared hasn't 3");
         }
+        //todo end task 88
+
+
 
     }
 }
