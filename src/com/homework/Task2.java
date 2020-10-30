5d7dc92deb9c42019763f665b7370da0408786b4
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-10-30 23:05:08 +0200

    resolve b)of the task2(88)

--- src/com/homework/Task2.java
+++ src/com/homework/Task2.java
@@ -3,21 +3,21 @@ package com.homework;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Task2 {
     public static void main(String[] args) throws IOException {
 
         int number;
         // Create InputStreamReader foEach number variable
-        BufferedReader valueOfNumber = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader valueOfNumber = new BufferedReader(
+                new InputStreamReader(System.in));
         System.out.print("Enter the value of n: ");
         number = Integer.parseInt(valueOfNumber.readLine());
         String numberToString = String.valueOf(number);
-        System.out.println("n is: " +number);
+        System.out.println("n is: " + number);
 
-        int powNumber = (int) Math.pow(number,2);
+        int powNumber = (int) Math.pow(number, 2);
         String sqrNumberToString = String.valueOf(powNumber);
         System.out.println("n^2 is: " + powNumber);
 
@@ -26,9 +26,14 @@ public class Task2 {
         } else {
             System.out.println("the number we had squared hasn't 3");
         }
-        //todo end task 88
 
-
-
+        char[] chars = numberToString.toCharArray();
+        System.out.println("Before Swaping 1st and last element :"
+                + Arrays.toString(chars));
+        char temp = chars[0];
+        chars[0] = chars[chars.length - 1];
+        chars[chars.length - 1] = temp;
+        System.out.println("After Swaping 1st and last element :"
+                + Arrays.toString(chars));
     }
 }
